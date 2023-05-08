4e373e6fe506f0b639f708a9a844c1e4d48ce00b
hassanShakur <hass.shakurhassan@gmail.com>
2023-05-08 17:53:39 +0300

    Initial commit

---
+++ basics.py
@@ -0,0 +1,21 @@
+name = input("Enter your name: ") 
+print("Hello " + name)
+
+# Conversions
+str(123)
+float("400")
+int("7")
+
+# Functions
+len('abcd')
+type(name)
+
+
+# Mathematics
+print(8 / 2) # FLoat result
+print(8 // 2) # int result
+
+round(1.456, 5) # Decimal places
+
+# F-string - formatted string
+print(f"Your name is {name}")
