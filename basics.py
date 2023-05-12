a5b41199ad6f2ef9dc7b9d137bc58a0c9892b53c
hassanShakur <hass.shakurhassan@gmail.com>
2023-05-12 17:07:42 +0300

    Logic ops & Leap year

--- basics.py
+++ basics.py
@@ -10,12 +10,27 @@ int("7")
 len('abcd')
 type(name)
 
+# Methods
+"tEXt".lower()
+'name'.count('a') # How many times 'a' occurs
+
 
 # Mathematics
 print(8 / 2) # FLoat result
 print(8 // 2) # int result
 
-round(1.456, 5) # Decimal places
+round(1.456, 5) # Decimal places precision
+print("{:.2f}".format(1.4)) # Add 0 to the end to fit num of decimal places
 
 # F-string - formatted string
 print(f"Your name is {name}")
+
+# Conditions
+if name or 1:
+    print('🫡✅')
+elif 1 == 1 and 3 == 3:
+    print('🟰')
+elif not 2 < 3:
+    print('Used not')
+else:
+    print('❌')
